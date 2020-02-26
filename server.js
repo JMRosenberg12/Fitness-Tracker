b739685ab2b70eea9dd984e28eae1297b21329c1
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-02-25 21:45:30 -0600

    add

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ app.use(express.static("Develop/public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
+// connect to the mongoDB 
+mongoose.connect(MONGODB_URI)
+
   
 
 // routes
